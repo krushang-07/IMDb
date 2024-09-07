6ca12f6782b2cbbda1921b7d30e06e79ab19c46c
KRUSHANG SAVALIYA <krushangsavaliya45@gmail.com>
2024-09-08 00:13:56 +0530

    fetching data through TMDB API Done!!

---
+++ src/Components/Card.jsx
@@ -0,0 +1,5 @@
+export default function Card({ result }) {
+  return (
+    <div className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200"></div>
+  );
+}
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,7 +4,7 @@ import NavBarItem from "./NavBarItem";
 export default function NavBar() {
   return (
     <div className="flex justify-center gap-6 text-lg dark:bg-amber-400 bg-amber-300 py-3">
-      <NavBarItem title="Tranding" param="fetchTrending" />
+      <NavBarItem title="Trending" param="fetchTrending" />
       <NavBarItem title="Top Rated" param="fetchTopRated" />
     </div>
   );
